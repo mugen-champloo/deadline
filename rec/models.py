c2b37bdc1e54a683b44dab0bdc4f9a4b6556d0b0
mugen_champloo <abdurahimmahau@gmail.com>
2023-07-28 01:54:49 +0600

    modified models

--- rec/models.py
+++ rec/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 
-# class Genre(models.Model):
-#     name = models.CharField(max_length=100, db_index=True, verbose_name="Жанр")
-#     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
-
-#     def __str__(self):
-#         return self.name
-    
-#     def get_absolute_url(self):
-#         return reverse('genre', kwargs={'genre_slug': self.slug})
-    
 class Director(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Режисер')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
@@ -22,16 +12,24 @@ class Director(models.Model):
     def get_absolute_url(self):
         return reverse('director', kwargs={'director_slug': self.slug})
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Жанр")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('genre', kwargs={'genre_slug': self.slug})
+    
 class Movie(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    genre = models.CharField(max_length=200, verbose_name='Жанр')
-    director = models.ForeignKey('Director', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
     year = models.IntegerField(blank=True)
     image = models.ImageField(null=True, blank=True)
     info = models.TextField(blank=True, verbose_name='Инфо')
     grade_kinopoisk = models.FloatField(blank=True)
     grade_imdb = models.FloatField(blank=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
 
 
     def __str__(self):
@@ -48,3 +46,18 @@ class Movie(models.Model):
         except:
             url = ''
         return url
+
+class MovieGenre(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True, blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.movie
+    
+class MovieDirector(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True, blank=True)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True, blank=True)
+   
+    def __str__(self):
+        return self.name
+    
